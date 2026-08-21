f2779fbbf2a33464445b6e8fac69f52591d41346
BautistaAraki <Bauti.araki@gmail.com>
2026-08-20 21:05:03 -0300

    feat:add initial game domain model

--- src/main/java/com/gamehub/gamehub/mode1/Game.java
+++ src/main/java/com/gamehub/gamehub/mode1/Game.java
@@ -1,5 +1,16 @@
 package com.gamehub.gamehub.mode1;
-
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+@Entity
 public class Game {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name ;
+    @Column(unique=true)
+    private Long steamAppID;
+    private String imageUrl;
 }
